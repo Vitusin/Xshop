20d6e24634d150f9eabc041503a897b292e33b4c
Vít Heřt <104905581+Vitusin@users.noreply.github.com>
2023-12-30 15:30:30 +0100

    Refactor App.js to include Product component and render a list of products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-
-import CategoryInfoDesktop from "./components/CategoryInfoDesktop.js";
+import Product from "./components/product";
+import CategoryInfoDesktop from "./components/CategoryInfoDesktop";
 
 const App = () => {
+  const products = [
+    {
+      id: 1,
+      name: "Produkt 1",
+      price: "30 Kč",
+      image: "url_obrázku_produktu_1",
+    },
+    {
+      id: 2,
+      name: "Produkt 2",
+      price: "40 Kč",
+      image: "url_obrázku_produktu_2",
+    },
+    // další produkty...
+  ];
+
   return (
-    <div>
+    <div className="App">
       <CategoryInfoDesktop />
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 };
---
+++ src/components/product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+  return (
+    <div className="product">
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.price}</p>
+    </div>
+  );
+};
+
+export default Product;
